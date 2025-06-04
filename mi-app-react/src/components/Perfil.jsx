eeec50b1f3c35eff19288b59a26e85b3e7b93a9e
mari-1306 <maria.rendon1306@gmail.com>
2025-06-04 19:43:16 +0000

    feat: componente perfil con descripcion personalizada

---
+++ mi-app-react/src/components/Perfil.jsx
@@ -0,0 +1,22 @@
+export const Perfil = () => {
+  return (
+    <section>
+      <h2>Perfil Profesional</h2>
+      <p>
+        Soy un desarrollador Front-End en proceso de formación,
+         motivado por la creación de aplicaciones web interactivas y adaptables a distintos dispositivos. 
+         Me entusiasma aprender de forma continua y trabajar en equipo para encontrar soluciones a desafíos tanto técnicos como creativos.
+      </p>
+      <h3>Habilidades Destacadas:</h3>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React.js y Vite</li>
+        <li>Trabajo en equipo</li>
+        <li>Comunicación efectiva</li>
+        <li>Aprendizaje autónomo</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Perfil;
