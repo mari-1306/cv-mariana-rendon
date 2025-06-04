fd5a8292a542c20d5523196e260b5d94284da707
mari-1306 <maria.rendon1306@gmail.com>
2025-06-04 19:01:47 +0000

    mi primer commit

---
+++ mi-app-react/src/components/CabeceraCV.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Cabecera.css";
+const CabeceraCV = () => {
+  return (
+    <header>
+      <h1>Mariana Rendon Montoya</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+      <p>Teléfono: 3026494779</p>
+      <p>Correo: marii.ren0713@gmail.com</p>
+      <p>Ciudad: medellin, Colombia</p>
+    </header>
+  );
+}
+
+export default CabeceraCV;
