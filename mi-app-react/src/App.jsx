cbe82c67c6b0b26ca75e9be7df9b759f3b46baa0
mari-1306 <maria.rendon1306@gmail.com>
2025-07-06 00:11:40 +0000

    feat: implementación de useState para mostrar/ocultar habilidades

--- mi-app-react/src/App.jsx
+++ mi-app-react/src/App.jsx
@@ -1,49 +1,19 @@
-// src/App.jsx
-import React from "react";
-import {
-  cabecera,
-  perfil,
-  educacion,
-  experiencia,
-  stackTecnologias,
-  proyectos,
-  habilidades
-} from "./data/cvData";
-
-import CabeceraCV from "./components/CabeceraCV";
-import Perfil from "./components/Perfil";
-import Educacion from "./components/Educacion";
-import Experiencia from "./components/Experiencia";
-import StackTecnologias from "./components/StackTecnologias";
-import Proyectos from "./components/Proyectos";
-import Habilidades from "./components/Habilidades";
+import React, { useState } from 'react';
+import ToggleHabilidades from './components/ToggleHabilidades';
+import Habilidades from './components/Habilidades';
+import { cvData } from './data/cvData';
 
 function App() {
+  const [mostrarHabilidades, setMostrarHabilidades] = useState(true);
+
+  const toggleMostrar = () => {
+    setMostrarHabilidades(!mostrarHabilidades);
+  };
+
   return (
-    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
-      {/* Cabecera */}
-      <CabeceraCV
-        nombre={cabecera.nombre}
-        profesion={cabecera.profesion}
-      />
-
-      {/* Perfil */}
-      <Perfil texto={perfil.texto} />
-
-      {/* Educación */}
-      <Educacion estudios={educacion} />
-
-      {/* Experiencia */}
-      <Experiencia trabajos={experiencia} />
-
-      {/* Stack de Tecnologías */}
-      <StackTecnologias tecnologias={stackTecnologias} />
-
-      {/* Proyectos (nuevo componente) */}
-      <Proyectos proyectos={proyectos} />
-
-      {/* Habilidades (nuevo componente) */}
-      <Habilidades habilidades={habilidades} />
+    <div>
+      <ToggleHabilidades mostrar={mostrarHabilidades} toggleMostrar={toggleMostrar} />
+      {mostrarHabilidades && <Habilidades habilidades={cvData.habilidades} />}
     </div>
   );
 }
